2cb0680051febf6c1ce9c606f225ddffce53be44
kyongseo <pokj930@naver.com>
2024-03-27 18:13:26 +0900

    commit2

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Test");
-        System.out.println("Exam");
+        System.out.println("Test3");
+        System.out.println("Exam3");
     }
 }
--- Test/Exam.java
+++ Test/Exam.java
@@ -3,6 +3,6 @@ package Test;
 
 public class Exam {
     public static void main(String[] args) {
-        System.out.println("Exam");
+        System.out.println("Exam3");
     }
 }
